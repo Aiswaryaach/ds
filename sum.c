52a4411e5f4335153ac4c5ce452713ff842f38b6
Aiswaryaach <aiswaryasudhakaran224@gmail.com>
2021-11-25 17:57:35 +0530

    third commit

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+printf("/n Enter the first number");
+scanf("%d",&a);
+printf("/n Enter the second number");
+scanf("%d",&b);
+sum=a+b;
+printf("sum = %d",sum);
+return(0);
+}
